9458706d2b36b593eceecbdbfdcca32bc87b4095
Nikolay Bardarov <152870144+NikolayBardarov22@users.noreply.github.com>
2025-12-26 17:02:18 +0200

    Implement controller decorator to improve the auth security Qos.

---
+++ CinemaApp/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿namespace CinemaApp.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Security.Claims;
+
+    [Authorize]
+    public abstract class BaseController : Controller
+    {
+        protected bool IsUserAutenticated()
+        {
+            bool retRes = false;
+            if (this.User.Identity != null)
+            {
+                retRes = this.User.Identity.IsAuthenticated;
+            }
+            return retRes;
+        }
+        protected string? GetUserId()
+        {
+            string? userId = null;
+            if (this.IsUserAutenticated())
+            {
+                userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+            return userId;
+        }
+    }
+}
